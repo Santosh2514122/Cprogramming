765b7d35597da0ec9ec5ad32f9cb554bb4d2b282
Santosh Yadav <santoshgkp107@gmail.com>
2025-01-17 15:51:35 +0530

    Add files via upload

---
+++ BASIC INPUT_OUTPUT/9evenOrOdd.c
@@ -0,0 +1,10 @@
+//find odd or even number using turnary operater (?)
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number:");
+scanf("%d",&num);
+(num%2==0)?printf("even number"):printf("odd number");
+return 0;
+}
