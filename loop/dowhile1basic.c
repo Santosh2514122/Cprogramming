942e1bb52a5fea4783976054cadf6f965619de82
Santosh Yadav <santoshgkp107@gmail.com>
2025-02-21 16:49:57 +0530

    Add files via upload

---
+++ loop/dowhile1basic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+
+    // variable declaration and initialization
+    int i = 0;
+  
+    // do while loop
+    do {
+        printf("santosh\n");
+        i++;
+    } while (i < 3);
+
+    return 0;
+}
---
+++ loop/dowhile2printmatrix.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    // Declaring  variables
+    int i = 0, j;
+    int c = 0;
+
+    // Outer loop starts
+    do {
+        j = 0;
+
+        // inner loop starts
+        do {
+            printf("%d  ", c++);
+            j++;
+        } while (j < 3);
+        printf("\n");
+        i++;
+    } while (i < 3);
+    return 0;
+}
