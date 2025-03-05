71fac0a7954da6707b32dd86baa98b4ae646c800
Santosh Yadav <santoshgkp107@gmail.com>
2025-03-05 16:10:02 +0530

    Add files via upload

---
+++ BASIC INPUT_OUTPUT/Transpose.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[3][3], transpose[3][3], i, j;
+
+    // Input matrix
+    printf("Enter elements of 3x3 matrix:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Transposing matrix
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    // Printing transpose
+    printf("Transpose of matrix:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
