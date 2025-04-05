3c3c14ce9e95c5ed520779a33e899ba6d07b2413
Santosh Yadav <santoshgkp107@gmail.com>
2025-04-05 23:50:41 +0530

    Update 2findtheLarge.c

--- day2/2findtheLarge.c
+++ day2/2findtheLarge.c
@@ -1,8 +1,7 @@
 //find the largest nubmer 
 #include<stdio.h>
 int main()
-{
-    int a,b,c;
+{int a,b,c;
     printf("enter the  three number:");
     scanf("%d%d%d",&a,&b,&c);
     int greater=(a>b&&b>c)?a:(b>c)?b:c;
