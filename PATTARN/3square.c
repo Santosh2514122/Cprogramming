f53e3fae7cfe3ab0c3487149abf3653cf7789e62
Santosh Yadav <santoshgkp107@gmail.com>
2025-04-10 23:52:23 +0530

    Update 3square.c

--- PATTARN/3square.c
+++ PATTARN/3square.c
@@ -1,8 +1,7 @@
 
 #include<stdio.h>
 int main()
-{
-    int n;
+{int n;
 printf("enter the row:");
 scanf("%d",&n);
 for(int  i=n;i>=1;i--)
@@ -11,7 +10,6 @@ for( int j=n;j>=1;j--)
 {
 printf("%d ",j);
 }
-
 printf("\n");
 }
 }
