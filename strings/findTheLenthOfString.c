f0da62274d1e4ffc1efa763d2831a12016f6fc04
Santosh Yadav <santoshgkp107@gmail.com>
2025-07-07 23:51:51 +0530

    Update findTheLenthOfString.c

--- strings/findTheLenthOfString.c
+++ strings/findTheLenthOfString.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 int main()
-{
-   char str[]=" santosh";
+{  char str[]=" santosh";
    int length= strlen(str);
    printf("length of string:%d",length);
    return 0;
